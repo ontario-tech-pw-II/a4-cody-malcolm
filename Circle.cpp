caeba32e37af7115fc72ac7fc42c40a5db7b37ed
cdmlclm <cdmlclm@hotmail.com>
2020-02-14 09:20:03 -0500

    complete activity

--- Circle.cpp
+++ Circle.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+
+const double PI = 3.14159;
+
 using namespace std;
 
 class Circle {
-  private:  
+  private:
     int x,y;      // center of circle
     double r;    // radius of circle
 
   public:
-   
+
     Circle(int =0,int=0, double =1.0);
     void print() const;
     double area() const;
@@ -23,46 +26,50 @@ class Circle {
 
 Circle::Circle(int x,int y, double r)
 {
-  // Initialize the object here
+  this->x = x;
+  this->y = y;
+  this->r = r;
 }
 
 double Circle::area() const {
-  // Return the area of a circle  
+  return PI*r*r;
 }
 
 double Circle::operator+(const Circle & c) const
 {
-   // + Return the area of two circles  
-
+   return this->area() + c.area();
 }
 
 Circle & Circle::operator++()
 {
-  // Increment the radius by 1
-
+  r++;
   return *this;
 }
 
 Circle Circle::operator++(int)
 {
   Circle temp = *this;
-  
-  //increment the radius by 1
-
+  r++;
   return temp;
 }
 
 ostream & operator<<(ostream &out, const Circle &c)
 {
 
-  // print a circle in the out stream (the format is the same as print())
+  out << "Circle =(" << c.x << "," << c.y << "," << c.r << ")";
   return out;
 }
 
 istream & operator>>(istream &in, Circle &c)
 {
- 
- // Read a circle information form the in stream
+ cout << "x: ";
+ in >> c.x;
+
+ cout << "y: ";
+ in >> c.y;
+
+ cout << "r: ";
+ in >> c.r;
 
   return in;
 }
@@ -77,23 +84,23 @@ void Circle::print() const
 
 int main()
 {
-  // Circle a;
-  // Circle b(2,3,3.0);
+  Circle a;
+  Circle b(2,3,3.0);
 
-  // a.print();
-  // b.print();
-  
-  // cout << "The area a + b is: " << a+b << endl;
-  // ++a;
-  // a.print();
+  a.print();
+  b.print();
 
-  // a++;
-  // a.print();
+  cout << "The area a + b is: " << a+b << endl;
+  ++a;
+  a.print();
 
-  // cout << a;
+  a++;
+  a.print();
 
-  // cin >> a;
-  // cout <<a;
+  cout << a;
+
+  cin >> a;
+  cout <<a;
 
   return 0;
 
